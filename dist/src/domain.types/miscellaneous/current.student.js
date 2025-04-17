e08b072a60a214bbd246f8d515546c03ccd54912
Shubham109879 <159152749+Shubham109879@users.noreply.github.com>
2025-04-17 17:27:21 +0530

    Delete dist directory

--- dist/src/domain.types/miscellaneous/current.student.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// import { Roles } from "../../database/sql/sequelize/models/student.model";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=current.student.js.map
--- dist/src/domain.types/student/student.domain.type.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=student.domain.type.js.map
